ed1464dcd96adc795e5bf22e04e6207583929147
DevLeonardoFA <leonardo.fernandes456@gmail.com>
2025-01-23 22:10:45 +0100

    SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,5 +6,18 @@ export default defineNuxtConfig({
   // load scss file globally
   css: ['~/assets/scss/global.scss'],
 
+  // seo
+  app: {
+    head: {
+      title: 'Leonardo F. Alonso | Portifolio',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'My Portifolio' },
+        { name: 'format-detection', content: 'telephone=no' },
+      ],
+    }
+  },
+
   modules: ['nuxt-aos']
 })
